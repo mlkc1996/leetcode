45bf4ea61e988aee295ff45ea08bcc02b4350dc8
Michael Lee <michaellee@myndar.com>
2024-06-27 15:32:44 +0800

    refactor

--- 345-Reverse Vowels of a String.js
+++ 345-Reverse Vowels of a String.js
@@ -3,39 +3,42 @@
  * @return {string}
  */
 var reverseVowels = function (s) {
-    const vowels = new Set(["a", "e", "i", "o", "u"]);
     const chars = s.split("");
     let left = 0;
     let right = s.length - 1;
-    while (left !== right) {
-        if (vowels.has(chars[left].toLowerCase()) && vowels.has(chars[right].toLowerCase())) {
+    while (left < right) {
+        const left_vowel = isVowel(chars[left]);
+        const right_vowel = isVowel(chars[right]);
+        if (left_vowel && right_vowel) {
             [chars[left], chars[right]] = [chars[right], chars[left]];
             left++;
-            if (left === right) {
-                break;
-            }
             right--;
-            if (left === right) {
-                break;
-            }
             continue;
         }
-        if (!vowels.has(chars[left].toLowerCase())) {
+        if (!left_vowel) {
             left++;
         }
-
-        if (left === right) {
-            break;
-        }
-
-        if (!vowels.has(chars[right].toLowerCase())) {
+        if (!right_vowel) {
             right--;
-        }
-
-        if (left === right) {
-            break;
         }
     }
 
     return chars.join("");
+};
+
+const isVowel = (c) => {
+    switch (c) {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+        case "A":
+        case "E":
+        case "I":
+        case "O":
+        case "U":
+            return true;
+        default: return false;
+    }
 };
