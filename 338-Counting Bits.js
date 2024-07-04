1402a8e9bff68301ed27caa573bcb943d82eee50
Michael Lee <michaellee@myndar.com>
2024-07-04 12:42:14 +0800

    338

---
+++ 338-Counting Bits.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function (n) {
+    const rtn = [];
+    let last;
+    for (let loop = 0; loop < n; loop++) {
+        let count = 1;
+        if (loop === 0) {
+            count = 0;
+        } else if (loop > 1) {
+            const log = Math.log2(loop);
+            if (Number.isInteger(log)) {
+                last = loop;
+            } else {
+                const diff = loop - last;
+                count += rtn[diff];
+            }
+        }
+        rtn.push(count);
+    }
+    return rtn;
+};
