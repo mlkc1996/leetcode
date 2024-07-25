c0cbd0aff9749cc608e629422858e1ebb172d9e6
mlkc1996 <mlkc1996@gmail.com>
2024-07-25 20:20:20 +0800

    binary search get the index to insert if not match

--- binarySearch.js
+++ binarySearch.js
@@ -1,25 +1,42 @@
 const binarySearch = (arr, target) => {
-    let low = 0;
-    let high = arr.length - 1;
-    while (low <= high) {
-        const half = Math.ceil((low + high) / 2);
-        const num = arr[half];
-        if (num === target) {
-            return half;
-        }
-        if (num > target) {
-            high = half - 1;
-        } else {
-            low = half + 1;
-        }
+  let low = 0;
+  let high = arr.length - 1;
+  while (low <= high) {
+    const half = Math.ceil((low + high) / 2);
+    const num = arr[half];
+    if (num === target) {
+      return half;
     }
-    console.log(low);
-    return - 1;
+    if (num > target) {
+      high = half - 1;
+    } else {
+      low = half + 1;
+    }
+  }
+  return -1;
 };
 const arr = [
-    5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
-    40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37, 40, 44, 64, 79, 84, 86, 95, 96, 98, 99,
 ];
 
 const target = 100;
 console.log(binarySearch(arr, target));
+
+const binarySearch2 = (arr, predicate) => {
+  let low = 0;
+  let high = arr.length;
+  while (low < high) {
+    const half = Math.ceil((low + high) / 2) - 1;
+    switch (predicate(arr[half])) {
+      case 0:
+        return half;
+      case 1:
+        low = half + 1;
+        break;
+      case -1:
+        high = half - 1;
+        break;
+    }
+  }
+  return high;
+};
